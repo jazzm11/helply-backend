27a0f9dc793517cd58de77c66815d9b4d6d83f88
Jazz Maquiraya <jazzm11>
2026-02-06 13:27:09 +0100

    Make a configuration for MongoDB - BACKEND

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require("express");
-
 const app = express();
 
+// App setup
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+require('dotenv').config();
+
+// Connect to MongoDB
+const connectDB = require("./config/db");
+connectDB(process.env.MONGO_URI);
+
+// Import routes
 const default_routes = require("./routes/default_routes");
 
-app.use(express.urlencoded({extended:true}));
-
-app.use(express.json());
-
+// Use routes
 app.use(default_routes)
 
+// Start the server
 app.listen(6001, ()=>{
     console.log("Server running on port 6001");
 })
