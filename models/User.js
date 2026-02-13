65ec6d518cbb93baefe9c26e9ae93da12a9edf60
Jazz Maquiraya <jazzm11>
2026-02-13 12:33:56 +0100

    Backend formatted

--- models/User.js
+++ models/User.js
@@ -1,48 +1,47 @@
-const {Schema, model}= require("mongoose")
-const argon2 = require("argon2")
-
+const { Schema, model } = require("mongoose");
+const argon2 = require("argon2");
 
 const userSchema = new Schema({
-    name:{
-        type: String,
-        required:true,
-        unique:true
-    },
-    passwd:{
-        type: String,
-        required: true,
-        unique:true
-    }
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passwd: {
+    type: String,
+    required: true,
+    unique: true,
+  },
 });
 
-userSchema.pre("save", async function(){
-    try{
-        this.passwd = await argon2.hash(this.passwd)
-    }catch(err){
-        console.log("Couldn't hash the password. Cause:", err)
-    }
-})
+userSchema.pre("save", async function () {
+  try {
+    this.passwd = await argon2.hash(this.passwd);
+  } catch (err) {
+    console.log("Couldn't hash the password. Cause:", err);
+  }
+});
 
-userSchema.statics.signUp = async(name, passwd, conPass)=>{
-if(passwd === conPass){
+userSchema.statics.signUp = async (name, passwd, conPass) => {
+  if (passwd === conPass) {
     const user = new User({
-        name:name,
-        passwd:passwd
-    })
-    await user.save()
-    return user._id
-}
-throw Error("The passwords doesn't match")
-}
+      name: name,
+      passwd: passwd,
+    });
+    await user.save();
+    return user._id;
+  }
+  throw Error("The passwords doesn't match");
+};
 
-userSchema.statics.signIn = async(name, passwd)=>{
-    const user = await User.find({name: name})
-    if(await argon2.verify(user.passwd, passwd)){
-        return user._id
-    }
-    throw Error("The provided password is wrong")
-}
+userSchema.statics.signIn = async (name, passwd) => {
+  const user = await User.find({ name: name });
+  if (await argon2.verify(user.passwd, passwd)) {
+    return user._id;
+  }
+  throw Error("The provided password is wrong");
+};
 
 const User = model("Users", userSchema);
 
-module.exports = User
+module.exports = User;
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
-const controller = require("../controllers/userController")
+const controller = require("../controllers/userController");
 
-router.post("/signUp",controller.sign_up_user)
+router.post("/signUp", controller.sign_up_user);
 
-router.post("/signIn",controller.sign_in_user)
+router.post("/signIn", controller.sign_in_user);
 
-module.exports = router
+module.exports = router;
